7f2a6c6905adca85275757ebb62e75cf563b6e3e
ewajs <ezequiel.wajs@gmail.com>
2018-11-06 17:02:58 -0300

    added test page

--- app.py
+++ app.py
@@ -5,4 +5,8 @@ app = Flask(__name__)
 def hello():
     return "Hello Mauro!"
 
+@app.route("/test")
+def test():
+    return "<h1>This is a test!</h1>"
+
 app.run(debug=True, host='0.0.0.0', port=8080)
